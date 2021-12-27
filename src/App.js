21b7e329cd8a90f11e8bd3809a36dae25719ce64
Tabby Douglass <tabbytoney@gmail.com>
2021-12-27 17:46:58 -0600

    New wrapper component to create cards

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   // just used localhost:3000 for this project so far
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Switch>
         <Route path="/" exact>
           <AllMeetupsPage />
@@ -21,7 +20,7 @@ function App() {
           <FavoritesPage />
         </Route>
       </Switch>
-    </div>
+    </Layout>
   );
 }
 
